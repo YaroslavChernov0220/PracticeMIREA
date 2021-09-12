e72bae239a434fbebdc50ac1331d876884adb014
Yaroslav Chernov <yaroslav_chernov1412@mail.ru>
2021-09-13 00:12:54 +0300

    Practice

--- src/ru/mirea/Practice2/MainAuthor.java
+++ src/ru/mirea/Practice2/MainAuthor.java
@@ -4,7 +4,7 @@ public class MainAuthor {
     public static void main(String[] args) {
         Author one =  new Author( "Tan Ah Teck",'M',"ahTeck@somewhere.com");
         Author two =  new Author( "Sue Grant",'F',"suGrant@somewhere.com");
-        Author thr =  new Author( "Elliot Page",'U',"ellpage@somewhere.com");
+        Author thr =  new Author( "Ella Page",'U',"ellpage@somewhere.com");
         System.out.println(one);
         System.out.println(two);
         System.out.println(thr);
