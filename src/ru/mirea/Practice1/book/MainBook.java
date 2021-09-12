b61473d5adbaecb1c14b9b140bf42403ccd8c9f9
Yaroslav Chernov <yaroslav_chernov1412@mail.ru>
2021-09-12 17:35:18 +0300

    Practice1

--- src/ru/mirea/Practice1/book/MainBook.java
+++ src/ru/mirea/Practice1/book/MainBook.java
@@ -2,7 +2,6 @@ package ru.mirea.Practice1.book;
 
 public class MainBook {
     public static void main(String[] args) {
-
         Book b1 = new Book("Собачье сердце", "М.А. Булгаков", 54, 500.9);
         System.out.println(b1);
         b1.getInfoBook();
