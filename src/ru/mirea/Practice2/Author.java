671432627554a5e3ba7e7c403b18776c29cf02f0
Yaroslav Chernov <yaroslav_chernov1412@mail.ru>
2021-09-12 22:16:11 +0300

    Practice2

---
+++ src/ru/mirea/Practice2/Author.java
@@ -0,0 +1,39 @@
+package ru.mirea.Practice2;
+
+public class Author {
+    private String name;
+    private String email;
+    private char gender;
+
+    public Author(String name, char gender, String email) {
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String toString() {
+        String g = new String("m");
+        if (gender == 'F')
+            g = "ms";
+        else if (gender == 'U')
+            g = "unknown";
+
+        return   name +  " (" + g + ") " + "at " +  email;
+    }
+}
---
+++ src/ru/mirea/Practice2/MainAuthor.java
@@ -0,0 +1,12 @@
+package ru.mirea.Practice2;
+
+public class MainAuthor {
+    public static void main(String[] args) {
+        Author one =  new Author( "Tan Ah Teck",'M',"ahTeck@somewhere.com");
+        Author two =  new Author( "Sue Grant",'F',"suGrant@somewhere.com");
+        Author thr =  new Author( "Elliot Page",'U',"ellpage@sjw.com");
+        System.out.println(one);
+        System.out.println(two);
+        System.out.println(thr);
+    }
+}
