4b807419230236c4d7e3ca676c44bb623b86a513
Yaroslav Chernov <yaroslav_chernov1412@mail.ru>
2021-10-17 15:51:36 +0300

    Practice

--- src/ru/mirea/Practice10/Task2/Chair/MagicChair.java
+++ src/ru/mirea/Practice10/Task2/Chair/MagicChair.java
@@ -4,7 +4,7 @@ import ru.mirea.Practice10.Task2.Chair.Chair;
 
 public class MagicChair implements Chair {
     public void doMagic(){
-        System.out.println("**Magic**");
+        System.out.println("Magic");
     }
 
     public MagicChair() {
